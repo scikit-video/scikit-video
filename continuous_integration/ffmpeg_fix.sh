a264882be233301506e879af401b011901f31d32
Todd <tgoodall@utexas.edu>
2015-12-10 02:32:27 -0600

    update

---
+++ continuous_integration/ffmpeg_fix.sh
@@ -0,0 +1,11 @@
+cd $HOME/download
+wget --no-check-certificate http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 || exit 1
+tar xjf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+echo ./configure
+./configure --disable-yasm --disable-static --enable-shared --disable-doc --prefix="$HOME/ffmpeg_install" || exit 2
+echo make
+make -j4 || exit 3
+echo make install
+make install || exit 4
+cd $GIT_TRAVIS_REPO
