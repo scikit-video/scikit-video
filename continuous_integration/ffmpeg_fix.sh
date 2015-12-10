a65239d63e8478cd22862fc307e1dced4788c311
Todd <tgoodall@utexas.edu>
2015-12-10 14:03:15 -0600

    update

--- continuous_integration/ffmpeg_fix.sh
+++ continuous_integration/ffmpeg_fix.sh
@@ -6,7 +6,6 @@ echo ./configure
 ./configure --disable-yasm --disable-static --enable-shared --disable-doc --prefix="$HOME/builds" || exit 2
 echo make
 make -j4 || exit 3
-# waste of time, just fix the paths
-# echo make install
-# make install || exit 4
+echo make install
+make install || exit 4
 cd $TRAVIS_BUILD_DIR
