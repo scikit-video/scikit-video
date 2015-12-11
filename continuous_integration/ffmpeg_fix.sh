f1b95dfdef5918bb2951f78c4924bd31ab65d64e
Todd <tgoodall@utexas.edu>
2015-12-10 19:49:48 -0600

    update

--- continuous_integration/ffmpeg_fix.sh
+++ continuous_integration/ffmpeg_fix.sh
@@ -1,7 +1,13 @@
 cd $HOME/download
 wget --no-check-certificate "http://ffmpeg.org/releases/ffmpeg-$FFMPEG.tar.bz2" || exit 1
 tar xjf "ffmpeg-$FFMPEG.tar.bz2"
-cd ffmpeg
+
+if [[ "$FFMPEG" == "snapshot" ]]; then 
+    cd "ffmpeg-$FFMPEG"; 
+else
+    cd "ffmpeg";
+fi
+
 echo ./configure
 ./configure --disable-yasm --disable-static --enable-shared --disable-doc --prefix="$HOME/builds" || exit 2
 echo make
