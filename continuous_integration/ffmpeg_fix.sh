d68427b79ea5c88935ad658abd82952a93d2f216
Todd <tgoodall@utexas.edu>
2015-12-10 19:32:33 -0600

    updating ffmpeg

--- continuous_integration/ffmpeg_fix.sh
+++ continuous_integration/ffmpeg_fix.sh
@@ -1,6 +1,6 @@
 cd $HOME/download
-wget --no-check-certificate http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 || exit 1
-tar xjf ffmpeg-snapshot.tar.bz2
+wget --no-check-certificate "http://ffmpeg.org/releases/ffmpeg-$FFMPEG.tar.bz2" || exit 1
+tar xjf "ffmpeg-$FFMPEG.tar.bz2"
 cd ffmpeg
 echo ./configure
 ./configure --disable-yasm --disable-static --enable-shared --disable-doc --prefix="$HOME/builds" || exit 2
