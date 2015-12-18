acf358efe95a8522211cdcb64300233edf5c2a4a
Todd <beyondmetis@gmail.com>
2015-12-18 01:47:09 -0600

    Adding logos and misc tinkering with doc website

--- doc/conf.py
+++ doc/conf.py
@@ -114,11 +114,11 @@ html_theme_path = ['themes']
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'scikit-video'
+html_short_title = ''
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/scikit-video.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
