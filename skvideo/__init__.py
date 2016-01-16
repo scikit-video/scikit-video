e0788220d25129ecbc52098ecc8418d46750d85d
Todd <tgoodall@utexas.edu>
2016-01-15 20:58:54 -0600

    Adding try/except to version checking

--- skvideo/__init__.py
+++ skvideo/__init__.py
@@ -39,42 +39,49 @@ _FFMPEG_PATCH_VERSION = 0
 
 if ((which("ffmpeg") != None) and (which("ffprobe") != None)):
     _HAS_FFMPEG = 1
-    # grab program version string
-    version = check_output(["ffmpeg",  "-version"])
-    # only parse the first line returned
-    firstline = version.split('\n')[0]
-    # the 3rd element in this line is the version number
-    version = firstline.split(' ')[2].strip()
-    versionparts = version.split('.')
-    _FFMPEG_MAJOR_VERSION = int(versionparts[0])
-    _FFMPEG_MINOR_VERSION = int(versionparts[1])
-    if len(versionparts) > 2:
-        _FFMPEG_PATCH_VERSION = int(versionparts[2])
+    try:
+        # grab program version string
+        version = check_output(["ffmpeg",  "-version"])
+        # only parse the first line returned
+        firstline = version.split('\n')[0]
+        # the 3rd element in this line is the version number
+        version = firstline.split(' ')[2].strip()
+        versionparts = version.split('.')
+        _FFMPEG_MAJOR_VERSION = int(versionparts[0])
+        _FFMPEG_MINOR_VERSION = int(versionparts[1])
+        if len(versionparts) > 2:
+            _FFMPEG_PATCH_VERSION = int(versionparts[2])
+    except:
+        pass
 
 if ((which("avconv") != None) and (which("avprobe") != None)):
     _HAS_AVCONV = 1
-    # grab program version string
-    version = check_output(["avconv",  "-version"])
-    print version
-    # only parse the first line returned
-    firstline = version.split('\n')[0]
 
-    firstlineparts = firstline.split(' ')
+    try:
+        # grab program version string
+        version = check_output(["avconv",  "-version"])
+        print version
+        # only parse the first line returned
+        firstline = version.split('\n')[0]
 
-    # in older versions, the second word is "version", 
-    # else the version number starts with "v"
-    if firstlineparts[1].strip() == "version":
-        version = firstlineparts[2].split('-')[0]
-    else:
-        version = firstlineparts[1].split('-')[0]
-        # check for underscore
-        version = version.split('_')[0]
-        versionparts = version.split('.')
-        if versionparts[0][0] == 'v':
-            _LIBAV_MAJOR_VERSION = int(versionparts[0][1:])
+        firstlineparts = firstline.split(' ')
+
+        # in older versions, the second word is "version", 
+        # else the version number starts with "v"
+        if firstlineparts[1].strip() == "version":
+            version = firstlineparts[2].split('-')[0]
         else:
-            _LIBAV_MAJOR_VERSION = int(versionparts[0])
-            _LIBAV_MINOR_VERSION = int(versionparts[1])
+            version = firstlineparts[1].split('-')[0]
+            # check for underscore
+            version = version.split('_')[0]
+            versionparts = version.split('.')
+            if versionparts[0][0] == 'v':
+                _LIBAV_MAJOR_VERSION = int(versionparts[0][1:])
+            else:
+                _LIBAV_MAJOR_VERSION = int(versionparts[0])
+                _LIBAV_MINOR_VERSION = int(versionparts[1])
+    except:
+        pass
 
 if which("mediainfo") != None:
     _HAS_MEDIAINFO = 1
