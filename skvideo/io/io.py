a625afbef24825f1c94ef440961899a86c9beb4c
Todd <tgoodall@utexas.edu>
2016-01-21 13:41:15 -0600

    Adding grayscale conversion to vread/vreader

--- skvideo/io/io.py
+++ skvideo/io/io.py
@@ -18,8 +18,8 @@ def vwrite(fname, videodata, inputdict=None, outputdict=None, backend='ffmpeg', 
         Video file name.
 
     videodata : ndarray
-        ndarray of dimension (T, M, N, C), (T, M, N), (M, N, C), or (M, N), 
-        where T is the number of frames, M is the height, N is width, 
+        ndarray of dimension (T, M, N, C), (T, M, N), (M, N, C), or (M, N),
+        where T is the number of frames, M is the height, N is width,
         and C is number of channels.
 
     inputdict : dict
@@ -31,10 +31,13 @@ def vwrite(fname, videodata, inputdict=None, outputdict=None, backend='ffmpeg', 
         disk.
 
     backend : string
-        Program to use for handling video data. Only 'ffmpeg' and 'libav' are supported at this time.
+        Program to use for handling video data.
+        Only 'ffmpeg' and 'libav' are supported at this time.
 
     verbosity : int
-        Setting to 0 (default) disables all debugging output. Setting to 1 enables all debugging output. Useful to see if the backend is behaving properly. 
+        Setting to 0 (default) disables all debugging output.
+        Setting to 1 enables all debugging output.
+        Useful to see if the backend is behaving properly.
 
     Returns
     -------
@@ -71,7 +74,7 @@ def vwrite(fname, videodata, inputdict=None, outputdict=None, backend='ffmpeg', 
         raise NotImplemented
 
 
-def vread(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=None, backend='ffmpeg', verbosity=0):
+def vread(fname, height=0, width=0, num_frames=0, as_grey=False, inputdict=None, outputdict=None, backend='ffmpeg', verbosity=0):
     """Load a video from file entirely into memory.
 
     Parameters
@@ -80,27 +83,36 @@ def vread(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=Non
         Video file name, e.g. ``bickbuckbunny.mp4``
 
     height : int
-        Set the source video height used for decoding. Useful for raw inputs when video header does not exist.
+        Set the source video height used for decoding.
+        Useful for raw inputs when video header does not exist.
 
     width : int
-        Set the source video width used for decoding. Useful for raw inputs when video header does not exist.
+        Set the source video width used for decoding.
+        Useful for raw inputs when video header does not exist.
 
     num_frames : int
-        Only read the first the first `num_frames` number of frames from video. Setting `num_frames` to 
-        small numbers can significantly speed up video loading times.
+        Only read the first `num_frames` number of frames from video.
+        Setting `num_frames` to small numbers can significantly
+        speed up video loading times.
+
+    as_grey : bool
+        If true, only load the luminance channel of the input video.
 
     inputdict : dict
         Input dictionary parameters, i.e. how to interpret the input file.
 
     outputdict : dict
-        Output dictionary parameters, i.e. how to encode the data 
+        Output dictionary parameters, i.e. how to encode the data
         when sending back to the python process.
 
     backend : string
-        Program to use for handling video data. Only 'ffmpeg' and 'libav' are supported at this time.
+        Program to use for handling video data.
+        Only 'ffmpeg' and 'libav' are supported at this time.
 
     verbosity : int
-        Setting to 0 (default) disables all debugging output. Setting to 1 enables all debugging output. Useful to see if the backend is behaving properly. 
+        Setting to 0 (default) disables all debugging output.
+        Setting to 1 enables all debugging output.
+        Useful to see if the backend is behaving properly.
 
     Returns
     -------
@@ -126,12 +138,19 @@ def vread(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=Non
         if num_frames != 0:
             outputdict['-vframes'] = str(num_frames)
 
+        if as_grey:
+            outputdict['-pix_fmt'] = 'gray'
+
         reader = FFmpegReader(fname, inputdict=inputdict, outputdict=outputdict, verbosity=verbosity)
         T, M, N, C = reader.getShape()
 
         videodata = np.zeros((T, M, N, C), dtype=np.uint8)
         for idx, frame in enumerate(reader.nextFrame()):
-            videodata[idx, :, :, :] = frame 
+            videodata[idx, :, :, :] = frame
+
+        if as_grey: 
+            videodata = vshape(videodata[:, :, :, 0])
+
         return videodata
     elif backend == "libav":
         # check if FFMPEG exists in the path
@@ -148,14 +167,15 @@ def vread(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=Non
 
         videodata = np.zeros((T, M, N, C), dtype=np.uint8)
         for idx, frame in enumerate(reader.nextFrame()):
-            videodata[idx, :, :, :] = frame 
+            videodata[idx, :, :, :] = frame
         return videodata
 
     else:
         raise NotImplemented
 
-def vreader(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=None, backend='ffmpeg', verbosity=0):
-    """Load a video through the use of a generator. 
+
+def vreader(fname, height=0, width=0, num_frames=0, as_grey=False, inputdict=None, outputdict=None, backend='ffmpeg', verbosity=0):
+    """Load a video through the use of a generator.
 
     Parameters
     ----------
@@ -163,35 +183,43 @@ def vreader(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=N
         Video file name, e.g. ``bickbuckbunny.mp4``
 
     height : int
-        Set the source video height used for decoding. Useful for raw inputs when video header does not exist.
+        Set the source video height used for decoding.
+        Useful for raw inputs when video header does not exist.
 
     width : int
-        Set the source video width used for decoding. Useful for raw inputs when video header does not exist.
+        Set the source video width used for decoding.
+        Useful for raw inputs when video header does not exist.
 
     num_frames : int
-        Only read the first the first `num_frames` number of frames from video. Setting `num_frames` to 
-        small numbers can significantly speed up video loading times.
+        Only read the first `num_frames` number of frames from video.
+        Setting `num_frames` to small numbers can significantly speed up video loading times.
+
+    as_grey : bool
+        If true, only load the luminance channel of the input video.
 
     inputdict : dict
         Input dictionary parameters, i.e. how to interpret the input file.
 
     outputdict : dict
-        Output dictionary parameters, i.e. how to encode the data 
-        when sending back to the python process.
+        Output dictionary parameters, i.e. how to encode the data
+        between backend and python.
 
     backend : string
-        Program to use for handling video data. Only 'ffmpeg' and 'libav' are supported at this time.
+        Program to use for handling video data.
+        Only 'ffmpeg' and 'libav' are supported at this time.
 
     verbosity : int
-        Setting to 0 (default) disables all debugging output. Setting to 1 enables all debugging output. Useful to see if the backend is behaving properly. 
+        Setting to 0 (default) disables all debugging output.
+        Setting to 1 enables all debugging output.
+        Useful to see if the backend is behaving properly.
 
 
     Returns
     -------
     vid_gen : generator
-	returns ndarrays, shape (M, N, C) where 
-	M is frame height, N is frame width, and 
-	C is number of channels per pixel 
+        returns ndarrays, shape (M, N, C) where
+        M is frame height, N is frame width, and
+        C is number of channels per pixel
 
     ----------------
     plugin_args : keywords
@@ -206,7 +234,7 @@ def vreader(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=N
 
     if backend == "ffmpeg":
         # check if FFMPEG exists in the path
-        assert _HAS_FFMPEG, "Cannot find installation of real FFmpeg (which comes with ffprobe)."
+        assert _HAS_FFMPEG, "Cannot find installation of ffmpeg."
 
         if ((height != 0) and (width != 0)):
             inputdict['-s'] = str(width) + 'x' + str(height)
@@ -214,9 +242,16 @@ def vreader(fname, height=0, width=0, num_frames=0, inputdict=None, outputdict=N
         if num_frames != 0:
             outputdict['-vframes'] = str(num_frames)
 
+        if as_grey:
+            outputdict['-pix_fmt'] = 'gray'
+
         reader = FFmpegReader(fname, inputdict=inputdict, outputdict=outputdict, verbosity=verbosity)
         for frame in reader.nextFrame():
-            yield frame
+            if as_grey:
+                yield vshape(frame[:, :, :, 0])
+            else:
+                yield frame
+
     elif backend == "libav":
         # check if FFMPEG exists in the path
         assert _HAS_AVCONV, "Cannot find installation of libav."
