7fd8944f9dd72ffd1bd2c8ae62481905e77f7511
Todd <tgoodall@utexas.edu>
2016-01-24 22:39:02 -0600

    Adding libav to the ffmpeg build

--- scripts/ffmpeg_fix.sh
+++ scripts/ffmpeg_fix.sh
@@ -15,3 +15,20 @@ make -j4 || exit 3
 echo make install
 make install || exit 4
 cd $TRAVIS_BUILD_DIR
+
+# also build/install libav
+cd $HOME/download
+wget --no-check-certificate "https://libav.org/releases/libav-11.4.tar.gz" || exit 1
+tar xjf "libav-11.4.tar.gz"
+
+cd "libav-11.4"; 
+
+echo ./configure
+./configure --disable-yasm --disable-static --enable-shared --disable-doc --prefix="$HOME/builds" || exit 2
+echo make
+make -j4 || exit 3
+echo make install
+make install || exit 4
+cd $TRAVIS_BUILD_DIR
+
+# also build/install libav
