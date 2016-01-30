d06f255e75aac244dee53d92407cd7caf1950497
Todd <tgoodall@utexas.edu>
2016-01-29 20:55:54 -0600

    More install checks

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -21,6 +21,12 @@ python --version
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 sudo apt-get -qq install g++-4.8
+
+alias gcc='gcc-4.8'
+alias cc='gcc-4.8'
+alias g++='g++-4.8'
+alias c++='c++-4.8'
+
 cc -v
 exit
 source scripts/install_backend.sh
