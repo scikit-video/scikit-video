7e1c40141aa7c3e8c1f87706b3ec66e6862822f1
Todd <tgoodall@utexas.edu>
2016-02-02 20:40:52 -0600

    Adding measures

---
+++ skvideo/measure/__init__.py
@@ -0,0 +1,11 @@
+"""Measurement and quality assessment tools.
+
+"""
+
+from .ssim import *
+from .psnr import *
+
+__all__ = [
+    'ssim',
+    'psnr',
+]
--- skvideo/setup.py
+++ skvideo/setup.py
@@ -7,5 +7,6 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('tests')
     config.add_subpackage('motion')
     config.add_subpackage('utils')
+    config.add_subpackage('measure')
 
     return config
