14a7c84fa56c32eb7f4bbe057ce118cbff6ce8fd
Todd <tgoodall@utexas.edu>
2016-11-16 12:42:54 -0800

    Changing the scene code.
    Added new method, adding minimum scene length, refactoring names

--- skvideo/measure/scene.py
+++ skvideo/measure/scene.py
@@ -21,7 +21,7 @@ def _percentage_distance(canny_in, canny_out, r):
 
     return 1.0 - np.float(np.sum(E_1 & E_2))/np.float(np.sum(E_1))
 
-def _scenedet_edges(videodata, threshold):
+def _scenedet_edges(videodata, threshold, min_scene_len=2):
     # the first frame is always a new scene
     detected_scenes = [0]
     r = 6
@@ -49,13 +49,13 @@ def _scenedet_edges(videodata, threshold):
         # print "percentage: ", bt - at
         p = np.max((p_in, p_out))
 
-        if p > threshold:
+        if (p > threshold) and (t - detected_scenes[len(detected_scenes)-1] > min_scene_len):
             detected_scenes.append(t+1)
 
     return np.array(detected_scenes)
 
 
-def _scenedet_intensity(videodata, parameter1):
+def _scenedet_histogram(videodata, parameter1, min_scene_len=2):
     # the first frame is always a new scene
     detected_scenes = [0]
 
@@ -77,13 +77,32 @@ def _scenedet_intensity(videodata, parameter1):
         hist2 /= 256.0
         
         framediff = np.mean(np.abs(hist1 - hist2))
-        if framediff > parameter1:
+        if (framediff > parameter1) and (t - detected_scenes[len(detected_scenes)-1] > min_scene_len):
             detected_scenes.append(t+1)
 
     return np.array(detected_scenes)
 
 
-def scenedet(videodata, method='edges', parameter1=None):
+def _scenedet_intensity(videodata, parameter1, min_scene_len=2, colorspace='hsv'):
+
+    detected_scenes = [0]
+
+    numFrames, height, width, channels = videodata.shape
+
+    for t in range(0, numFrames-1):
+        frame0 = videodata[t].astype(np.float)
+        frame1 = videodata[t+1].astype(np.float)
+
+        delta = np.sum(np.abs(frame1 - frame0)/(height * width * channels))
+
+        if (delta > parameter1) and (t - detected_scenes[len(detected_scenes)-1] > min_scene_len):
+            detected_scenes.append(t+1)
+
+    return np.array(detected_scenes)
+
+
+
+def scenedet(videodata, method='histogram', parameter1=None, min_scene_length=2):
     """Scene detection algorithms
     
     Given a sequence of frames, this function
@@ -96,12 +115,17 @@ def scenedet(videodata, method='edges', parameter1=None):
         an input frame sequence, shape (T, M, N, C), (T, M, N), (M, N, C) or (M, N)
 
     method : string
-        "intensity" --> threshold-based (parameter1 defaults to 0.0625) approach using intensity histogram differences. [#f1]_
+        "histogram" --> threshold-based (parameter1 defaults to 1.0) approach using intensity histogram differences. [#f1]_
 
         "edges" --> threshold-based (parameter1 defaults to 0.5) approach measuring the edge-change fraction after global motion compensation [#f2]_
-   
+
+        "intensity" --> Detects fast cuts using changes in colour and intensity between frames. Parameter1 is the threshold used for detection, which defaults to 30.0.
+
     parameter1 : int
         Number used as a tuning parameter. See method argument for details.
+
+    min_scene_length : int
+        Number used for determining minimum scene length.
 
     Returns
     ----------
@@ -115,21 +139,22 @@ def scenedet(videodata, method='edges', parameter1=None):
 
     .. [#f2] Kevin Mai, Ramin Zabih, and Justin Miller. Feature-based algorithms for detecting and classifying scene breaks. Cornell University, 1995.
 
-
-    
-
     """
     videodata = vshape(videodata)
 
     detected_scenes = []
-    if method == "intensity":
+    if method == "histogram":
         if parameter1 is None:
-            parameter1 = 0.0625
-        detected_scenes = _scenedet_intensity(videodata, parameter1)
+            parameter1 = 1.0
+        detected_scenes = _scenedet_histogram(videodata, parameter1, min_scene_length)
     elif method == "edges":
         if parameter1 is None:
             parameter1 = 0.6
-        detected_scenes = _scenedet_edges(videodata, parameter1)
+        detected_scenes = _scenedet_edges(videodata, parameter1, min_scene_length)
+    elif method == "intensity":
+        if parameter1 is None:
+            parameter1 = 1.0
+        detected_scenes = _scenedet_intensity(videodata, parameter1, min_scene_length)
     else:
         raise NotImplementedError
 
