09309b25c19960a17ebef5029d5696a0a1f4a3a0
Todd <tgoodall@utexas.edu>
2017-06-01 22:31:46 -0700

    Documentation update for NIQE, change+bump to circle

--- skvideo/measure/niqe.py
+++ skvideo/measure/niqe.py
@@ -90,6 +90,30 @@ def _get_patches_generic(img, patch_size, is_train, stride):
     return feats
 
 def niqe(inputVideoData):
+    """Computes Naturalness Image Quality Evaluator. [#f1]_
+
+    Input a video of any quality and get back its distance frame-by-frame
+    from naturalness.
+
+    Parameters
+    ----------
+    inputVideoData : ndarray
+        Input video, ndarray of dimension (T, M, N, C), (T, M, N), (M, N, C), or (M, N),
+        where T is the number of frames, M is the height, N is width,
+        and C is number of channels. Here C is only allowed to be 1.
+
+    Returns
+    -------
+    niqe_array : ndarray
+        The niqe results, ndarray of dimension (T,), where T
+        is the number of frames
+
+    References
+    ----------
+
+    .. [#f1] Mittal, Anish, Rajiv Soundararajan, and Alan C. Bovik. "Making a 'completely blind' image quality analyzer." IEEE Signal Processing Letters 20.3 (2013): 209-212.
+
+    """
     # cache
     patch_size = 96
     module_path = dirname(__file__)
