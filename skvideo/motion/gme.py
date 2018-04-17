30ef46c0c877cde0010928db0f6ec95611aac970
sglidq625 <sglidq625@gmail.com>
2018-04-17 17:50:23 +0800

    fix bugs in numpy boolean substract
    fix bugs in numpy boolean substract

--- skvideo/motion/gme.py
+++ skvideo/motion/gme.py
@@ -16,8 +16,8 @@ def _hausdorff_distance(E_1, E_2):
     diamond = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
 
     # extract only 1-pixel border line of objects
-    E_1_per = E_1 - scipy.ndimage.morphology.binary_erosion(E_1, structure=diamond)
-    E_2_per = E_2 - scipy.ndimage.morphology.binary_erosion(E_2, structure=diamond)
+    E_1_per = E_1^scipy.ndimage.morphology.binary_erosion(E_1, structure=diamond)
+    E_2_per = E_2^scipy.ndimage.morphology.binary_erosion(E_2, structure=diamond)
 
     A = scipy.ndimage.morphology.distance_transform_edt(~E_2_per)[E_1_per].max()
     B = scipy.ndimage.morphology.distance_transform_edt(~E_1_per)[E_2_per].max()
