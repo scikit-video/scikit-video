a99473e273e3c8d27c6d95c65020acb5e17e6efc
John Henning <jlhbaseball15@me.com>
2018-08-16 10:15:14 -0400

    added iterator support for VideoReader

--- skvideo/tests/test_ffmpeg.py
+++ skvideo/tests/test_ffmpeg.py
@@ -15,8 +15,9 @@ else:
 
 @unittest.skipIf(not skvideo._HAS_FFMPEG, "FFmpeg required for this test.")
 def test_FFmpegReader():
-    reader = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), verbosity=0)
-    
+    reader = skvideo.io.FFmpegReader(
+        skvideo.datasets.bigbuckbunny(), verbosity=0)
+
     T = 0
     M = 0
     N = 0
@@ -38,11 +39,8 @@ def test_FFmpegReader():
 
     assert_equal(accumulation / (T * M * N * C), 109.28332841215979)
 
-
-@unittest.skipIf(not skvideo._HAS_FFMPEG, "FFmpeg required for this test.")
-def test_FFmpegReader_16bits():
-    reader16 = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), outputdict={'-pix_fmt':'rgb48le'}, verbosity=0)
-    reader8 = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), outputdict={'-pix_fmt':'rgb24'}, verbosity=0)
+    reader = skvideo.io.FFmpegReader(
+        skvideo.datasets.bigbuckbunny(), verbosity=0)
 
     T = 0
     M = 0
@@ -50,14 +48,45 @@ def test_FFmpegReader_16bits():
     C = 0
     accumulation = 0
 
+    for frame in reader:
+        M, N, C = frame.shape
+        accumulation += np.sum(frame)
+        T += 1
+
+    # check the dimensions of the video
+
+    assert_equal(T, 132)
+    assert_equal(M, 720)
+    assert_equal(N, 1280)
+    assert_equal(C, 3)
+
+    # check the numbers
+
+    assert_equal(accumulation / (T * M * N * C), 109.28332841215979)
+
+
+@unittest.skipIf(not skvideo._HAS_FFMPEG, "FFmpeg required for this test.")
+def test_FFmpegReader_16bits():
+    reader16 = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), outputdict={
+                                       '-pix_fmt': 'rgb48le'}, verbosity=0)
+    reader8 = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), outputdict={
+                                      '-pix_fmt': 'rgb24'}, verbosity=0)
+
+    T = 0
+    M = 0
+    N = 0
+    C = 0
+    accumulation = 0
 
     for frame8, frame16 in zip(reader8.nextFrame(), reader16.nextFrame()):
         # testing with the measure module may be a better idea but would add a dependency
 
         # check that there is no more than a 3/256th defference between the 8bit and 16 bit decoded image
-        assert(np.max(np.abs(frame8.astype('int32') - (frame16//256).astype('int32'))) < 4)
+        assert(np.max(np.abs(frame8.astype('int32') -
+                             (frame16//256).astype('int32'))) < 4)
         # then check that the mean difference is less than 1
-        assert(np.mean(np.abs(frame8.astype('float32') - (frame16//256).astype('float32'))) < 1.0)
+        assert(np.mean(np.abs(frame8.astype('float32') -
+                              (frame16//256).astype('float32'))) < 1.0)
         M, N, C = frame8.shape
         accumulation += np.sum(frame16//256)
         T += 1
@@ -76,8 +105,9 @@ def test_FFmpegReader_16bits():
 
 @unittest.skipIf(not skvideo._HAS_FFMPEG, "FFmpeg required for this test.")
 def test_FFmpegReader_fps():
-    reader = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), outputdict={"-r": "10"}, verbosity=0)
-    
+    reader = skvideo.io.FFmpegReader(skvideo.datasets.bigbuckbunny(), outputdict={
+                                     "-r": "10"}, verbosity=0)
+
     T = 0
     M = 0
     N = 0
